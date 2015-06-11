ec696e1ddfd4b69cc6335299358bcfa7dc9ee1b2
Nic Bell <nic.bell@hogarthww.com>
2015-06-11 12:17:17 +0100

    Updated to use new shimly

--- routes/index.js
+++ routes/index.js
@@ -4,28 +4,13 @@ var shimly = require('shimly');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Shimly', shims: [
-      'Array.every', 
-      'Array.filter', 
-      'Array.forEach', 
-      'Array.indexOf', 
-      'Array.map', 
-      'Array.some', 
-      'Element.classList', 
-      'Function.bind', 
-      'Object.assign', 
-      'JSON',
-      'localStorage',
-      'requestAnimationFrame',
-      'EventListener',
-      'Document.getElementsByClassName'
-    ] });
+  res.render('index', { title: 'Shimly', shims: shimly.list() });
 });
 
 
 /* POST returns shims. */
 router.post('/shim', function(req, res, next) {
-  var code = shimly(req.body.shims, req.body.minify);
+  var code = shimly.shim(req.body.shims, req.body.minify);
   
   res.setHeader('content-type', 'text/javascript');
   res.setHeader('content-disposition', 'attachment; filename=shimly.js');
