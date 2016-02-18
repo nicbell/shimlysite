8020dd4c433437172e0eb3b771569a0e7d9889b4
Nic Bell <me@nicbell.net>
2016-02-18 17:33:56 +0000

    fixed bug when only one shim is selected.

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,18 @@ var router = express.Router();
 var shimly = require('shimly');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Shimly', shims: shimly.list() });
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Shimly', shims: shimly.list() });
 });
 
 
 /* POST returns shims. */
-router.post('/shim', function(req, res, next) {
-  var code = shimly.shim(req.body.shims, req.body.minify);
-  
-  res.setHeader('content-type', 'text/javascript');
-  res.setHeader('content-disposition', 'attachment; filename=shimly.js');
-  res.end(code);
+router.post('/shim', function (req, res, next) {
+    var code = shimly.shim(req.body.shims.pop ? req.body.shims : [req.body.shims], req.body.minify);
+
+    res.setHeader('content-type', 'text/javascript');
+    res.setHeader('content-disposition', 'attachment; filename=shimly.js');
+    res.end(code);
 });
 
 module.exports = router;
